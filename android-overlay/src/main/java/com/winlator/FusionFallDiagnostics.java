a509b7bf6340ac01d2bb0beabb5ca44430fa8594
Rodrigo Sigrist <rsigristc@users.noreply.github.com>
2026-08-24 22:07:40 -0600

    fix: import diagnostic download exception

--- android-overlay/src/main/java/com/winlator/FusionFallDiagnostics.java
+++ android-overlay/src/main/java/com/winlator/FusionFallDiagnostics.java
@@ -29,6 +29,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
